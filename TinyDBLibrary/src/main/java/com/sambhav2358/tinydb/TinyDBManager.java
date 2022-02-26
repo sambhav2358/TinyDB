829706a153f6f51ff7149f98f9daefee9a0fcfc2
sambhav2358 <uni.app.world@gmail.com>
2022-02-26 12:15:30 +0530

    Made changes

--- TinyDBLibrary/src/main/java/com/sambhav2358/tinydb/TinyDBManager.java
+++ TinyDBLibrary/src/main/java/com/sambhav2358/tinydb/TinyDBManager.java
@@ -16,29 +16,65 @@ public class TinyDBManager {
         Paper.init(context);
     }
 
-    public void putString(String key, String value){ sharedPreferences.edit().putString(key,value).apply(); }
+    public void putString(String key, String value){
+        if (isNull(key)) throw new NullPointerException();
+        sharedPreferences.edit().putString(key,value).apply();
+    }
 
-    public void putInt(String key, int value){ sharedPreferences.edit().putInt(key,value).apply(); }
+    public void putInt(String key, int value){
+        if (isNull(key)) throw new NullPointerException();
+        sharedPreferences.edit().putInt(key,value).apply();
+    }
 
-    public void putFloat(String key, float value){ sharedPreferences.edit().putFloat(key,value).apply(); }
+    public void putFloat(String key, float value){
+        if (isNull(key)) throw new NullPointerException();
+        sharedPreferences.edit().putFloat(key,value).apply();
+    }
 
-    public void putBoolean(String key, boolean value){ sharedPreferences.edit().putBoolean(key,value).apply(); }
+    public void putBoolean(String key, boolean value){
+        if (isNull(key)) throw new NullPointerException();
+        sharedPreferences.edit().putBoolean(key,value).apply();
+    }
 
-    public <E> void putList(String key, List<E> value){ Paper.book().write(key,value); }
+    public <E> void putList(String key, List<E> value){
+        if (isNull(key)) throw new NullPointerException();
+        Paper.book().write(key,value);
+    }
 
-    public <E> void put(String key, E value){ Paper.book().write(key,value); }
+    public <E> void put(String key, E value){
+        if (isNull(key)) throw new NullPointerException();
+        Paper.book().write(key,value);
+    }
 
-    public String getString(String key, String defValue){return sharedPreferences.getString(key,defValue);}
+    public String getString(String key, String defValue){
+        if (isNull(key)) throw new NullPointerException();
+        return sharedPreferences.getString(key,defValue);
+    }
 
-    public int getInt(String key, int defValue){return sharedPreferences.getInt(key,defValue);}
+    public int getInt(String key, int defValue){
+        if (isNull(key)) throw new NullPointerException();
+        return sharedPreferences.getInt(key,defValue);
+    }
 
-    public float getFloat(String key, float defValue){return sharedPreferences.getFloat(key,defValue);}
+    public float getFloat(String key, float defValue){
+        if (isNull(key)) throw new NullPointerException();
+        return sharedPreferences.getFloat(key,defValue);
+    }
 
-    public boolean getBoolean(String key, boolean defValue){return sharedPreferences.getBoolean(key,defValue);}
+    public boolean getBoolean(String key, boolean defValue){
+        if (isNull(key)) throw new NullPointerException();
+        return sharedPreferences.getBoolean(key,defValue);
+    }
 
-    public <E> List<E> getList(String key, List<E> defValue){return Paper.book().read(key);}
+    public <E> List<E> getList(String key, List<E> defValue){
+        if (isNull(key)) throw new NullPointerException();
+        return Paper.book().read(key);
+    }
 
-    public <E> List<E> get(String key, E defValue){return Paper.book().read(key);}
+    public <E> List<E> get(String key, E defValue){
+        if (isNull(key)) throw new NullPointerException();
+        return Paper.book().read(key);
+    }
 
     public void clearAll(){
         sharedPreferences.edit().clear().apply();
@@ -46,7 +82,10 @@ public class TinyDBManager {
     }
 
     public void clearKey(String key){
+        if (isNull(key)) throw new NullPointerException();
         sharedPreferences.edit().remove(key).apply();
         Paper.book().delete(key);
     }
+
+    private <E> boolean isNull(E value){ return value == null; }
 }
